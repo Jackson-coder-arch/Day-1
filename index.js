5afc8c21340dba9c602a68084c64bc751bc6b433
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-01-26 05:38:53 +0300

    get countries

--- index.js
+++ index.js
@@ -1,121 +1,144 @@
-// //Declare a variable
-// let challenge ="30 Days of Javascript"
-// console.log(challenge)
 
-// //Print Length
-
-// console.log(challenge.length)
-
-// //Change to uppercase
-
-// console.log(challenge.toUpperCase());
-
-// //change to lowercase
-
-// console.log(challenge.toLowerCase())
-
-// //cut or slice
-
-// console.log(challenge.substring(0,14));
-
-// //slice 30 from 30 days of javascript
-// console.log(challenge.substring(0,2))
-// console.log(challenge.substring(2,22))
-
-// //check if the string has the word script
-// console.log(challenge.includes('script'))
-
-// //change to split into an array
-
-// console.log(challenge.split(''))
-
-// //split 30 days of javascript where there is space
-
-// console.log(challenge.split(','))
-
-// //use chartAt
-// console.log(challenge.chartAt(15))
-
-// //let app string be
-
-// // let apps =['facebook', 'Google','Microsoft','Apple','IBM','Oracle','Amazon']
-// // console.log(apps)
-// //split the array at the commas
-
-
-// console.log(challenge.charCodeAt())
 
 
 
 //callback
-const doSOmething =(callback) =>{
-    setTimeout(function(){
-        const skills = ['HTML','CSS','JS']
-        callback('It did not go well', skills)
-    },2000)
-}
-const callback =(err,result) => {
-    if (err){
-        return console.log(err)
-    }
-    return console.log(result)
-}
-doSOmething(callback)
+// const doSOmething =(callback) =>{
+//     setTimeout(function(){
+//         const skills = ['HTML','CSS','JS']
+//         callback('It did not go well', skills)
+//     },2000)
+// }
+// const callback =(err,result) => {
+//     if (err){
+//         return console.log(err)
+//     }
+//     return console.log(result)
+// }
+// doSOmething(callback)
 
 
-const doSomething = callback =>{
-    setTimeout(() =>{
-        const skills = ['HTML','CSS','JS']
-        callback(false, skills)
-    },1000)
-}
+// const doSomething = callback =>{
+//     setTimeout(() =>{
+//         const skills = ['HTML','CSS','JS']
+//         callback(false, skills)
+//     },1000)
+// }
 
-doSomething((err,result) =>{
-    if (err){
-        console.log(err)
-    }
-    return console.log(result)
-})
+// doSomething((err,result) =>{
+//     if (err){
+//         console.log(err)
+//     }
+//     return console.log(result)
+// })
 
 // Promise constructor
 //syntax
-const promise = new Promise((resolve,reject) =>{
-    resolve('success')
-    reject('failure')
-})
+// const promise = new Promise((resolve,reject) =>{
+//     resolve('success')
+//     reject('failure')
+// })
 
-//promise
-const doPromise = new Promise((resolve, reject) =>{
-    setTimeout(() =>{
-        const skills = ['HTML','CSS','JS']
-        if (skills.length >0){
-            resolve(skills)
-        } else {
-            reject('something wrong has happened')
-        }
+// //promise
+// const doPromise = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         const skills = ['HTML','CSS','JS']
+//         if (skills.length >0){
+//             resolve(skills)
+//         } else {
+//             reject('something wrong has happened')
+//         }
     
-    },2000)
-})
+//     },2000)
+// })
 // doPromise
 //  .then(result=>{
 //      console.log(result)
 //  })
 // .catch(error =>console.log(error))
 
-//the promise has been settled with resolve
-const doPromise = new Promise((resolve, reject) =>{
-    setTimeout(() =>{
-        const skills = ['HTML','CSS','JS']
-        if (skills.indexOf('Node') !== -1{
-            resolve('fullstack developer')
-        }else{
-            reject('something wrong has happened')
-        }
-    },2000)
- })
+// //the promise has been settled with resolve
+
+// const doPromise = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         const skills = ['HTML','CSS','JS']
+//         if (skills.indexOf('Node') !== -1 {
+//             resolve('fullstack developer')
+//         }else{
+//             reject('something wrong has happened')
+//         }
+//     },2000)
+//  })
  
- doPromise
- .then(result =>{
-     console.log(result)
- })
- .catch(error =>console.log(error))
+//  doPromise
+//  .then(result =>{
+//      console.log(result)
+//  })
+//  .catch(error =>console.log(error))
+
+
+//Fetch API for contries 
+
+
+
+// const fetchAPI =(url, condition) => {
+//     return new Promise(function(resolve,reject){
+//     if (condition){
+//         resolve('countries list')
+//     }else{
+//         reject('something went wrong')
+//     }
+// });
+// };
+
+
+// fetchAPI('h1')
+// .then(function(data){
+//     console.log(data);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+
+//List of countries
+
+
+const url = 'https://restcountries.eu/rest/v2/all'
+
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.then((data) =>{
+    console.log(data);
+})
+.catch(function(err){
+    console.log(err)
+})
+
+//list of cats
+
+
+const url1 = 'https://api.thecatapi.com/v1/breeds' 
+
+async function getCats(){
+    try{
+        const result = await fetch(url).then(function(result){
+            return result.json();
+        }) ;
+        return result;
+    } catch (error){
+        console.log(error);
+    }
+}
+
+// fetch (url1)
+// .then(function(response){
+//     return response.json();
+// })
+// .then((data){
+//     console.log(data)
+// })
+// .catch(function(err){
+//     console.log(err)
+// })
